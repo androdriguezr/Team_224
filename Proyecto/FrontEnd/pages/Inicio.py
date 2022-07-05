919f926177459fb3e53b8e55c72973c0314e61ff
juliandnp0 <juliandnp@gmail.com>
2022-07-04 21:14:47 -0500

    ji

---
+++ Proyecto/FrontEnd/pages/Inicio.py
@@ -0,0 +1,17 @@
+import dash_bootstrap_components as dbc
+from dash_labs.plugins.pages import register_page
+from dash import html
+
+register_page(__name__, path="/")
+
+layout=  dbc.Container(
+    [   html.Div(children=[dbc.Row(dbc.Col(html.H5("Seleccione el link de interes:"), style={'textAlign': 'center',"font-size": "40px"}))]),
+        dbc.Row([dbc.NavItem(dbc.NavLink("Antecedentes", href="/antecedentes"),className= "card2")
+        ]),
+        dbc.Row([dbc.NavItem(dbc.NavLink("Modelo", href="/modelo"),className= "card2")
+        ]),
+        dbc.Row([dbc.NavItem(dbc.NavLink("Dashboard", href="/dashboard"),className= "card2")
+        ]),
+        dbc.Row([ html.Img(src='/assets/logo.png',className="img-fluid")],  className = "card")
+    ]
+)  
